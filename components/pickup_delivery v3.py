8d1733f00181fb62532021b46e15f344434a01ce
George7387 <19173@my.sanctamaria.school.nz>
2022-02-18 14:42:25 +1300

    Created pickup or delivery menu
    Created different versions of menu refining it in version 4

--- components/pickup_delivery v3.py
+++ components/pickup_delivery v3.py
@@ -2,10 +2,10 @@
 
 # Bug - need to make it so that it only accepts 1 or 2
 
-print("Is your order pickup or delivery?")
+print ("Is your order pickup or delivery?")
 
-print("For pickup please enter 1")
-print("For delivery please enter 2")
+print ("For pickup please enter 1")
+print ("For delivery please enter 2")
 
 
 
@@ -15,15 +15,16 @@ while True:
     try:
         delivery = int(input ("Please enter a number")) 
         if delivery >= 1 and delivery <= 2:
-         if delivery == 1:
-            print("Pickup")
-            break
+            if delivery == 1:
+                print ("Pickup")
+                break
 
-        elif delivery == 2:
-            print("Delivery")
-            break
+            elif delivery == 2: 
+                print ("Delivery")
+                break
+
         else:
             print("The number must be 1 or 2")
     except ValueError: 
-            print("That is not a valid number")
+            print ("That is not a valid number")
             print("Please enter 1 or 2")
