e6120c7dd28c9e888f1cfabc525c20c351b5222b
George7387 <19173@my.sanctamaria.school.nz>
2022-02-22 12:11:27 +1300

    Added customer details dictionary
    Added a dictionary to store customer details
    Name
    Phone

--- components/pickup v3.py
+++ components/pickup v3.py
@@ -1,9 +1,10 @@
+# Customer details dictionary 
 customer_details = {}
 
-
+# Basic instructions
 print("Please enter the pickup information")
 
-#customer name
+#customer name not blank
 valid = False
 while not valid:
     customer_details['name'] = input("Please enter your name")
@@ -14,7 +15,7 @@ while not valid:
         print("Sorry this cannot be blank")
 
     
-#customer phone number  
+#customer phone number not blank
 valid = False
 while not valid:
     customer_details['phone'] = input("Please enter your phone number")
