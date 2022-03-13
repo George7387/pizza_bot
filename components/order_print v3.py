d55003215403d89e5363215a28202c4aef75766a
George7387 <19173@my.sanctamaria.school.nz>
2022-03-14 10:06:10 +1300

    Print_order Function
    Adding formatting to print_order and added total cost. Developed into a function

--- components/order_print v3.py
+++ components/order_print v3.py
@@ -3,6 +3,15 @@ order_list = ['Margherita', 'Hawaiian', 'Vegan', 'BBQ Chicken Deluxe']
 # List to store pizza prices
 order_cost = [8.50, 8.50, 8.50, 13.50]
 
+cust_details = {'name':'Mark','phone':'0213345656', 'house':'45', 'street':'Harry', 'suburb':'Howick'}
+
+
+
+print(f"Customer Name: {cust_details['name']} \nCustomer Phone: {cust_details['phone']} \nCustomer Address: {cust_details['house']} {cust_details['street']} {cust_details['suburb']}")
+
+
+
+
 
 count = 0
 for item in order_list:
